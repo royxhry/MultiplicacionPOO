73a67bd44de5a5d0d065d111ceb089af3cc9e56e
Rodrigo <hl>
2024-05-08 01:02:18 -0600

    v3

--- MainMultiplicacion.java
+++ MainMultiplicacion.java
@@ -2,26 +2,31 @@ import java.util.Scanner;
 
 public class MainMultiplicacion {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
         Scanner sc = new Scanner(System.in);
+        // Crear un objeto de tipo Multiplicacion
         Multiplicacion m1 = new Multiplicacion();
-        System.out.println("Ingresa el primer numero");
+        
+        // Solicitar al usuario que ingrese el primer número
+        System.out.println("Ingresa el primer número:");
         int a = sc.nextInt();
         m1.setA(a);
-        System.out.println("Ingresa el segundo numero");
+        
+        // Solicitar al usuario que ingrese el segundo número
+        System.out.println("Ingresa el segundo número:");
         int b = sc.nextInt();
         m1.setB(b);
 
+        // Realizar la multiplicación
         m1.multiplicar();
 
+        // Obtener el resultado de la multiplicación
         int r = m1.getR();
-        System.out.println("EL resultado de la multiplicacion es:"+r);
+        System.out.println("El resultado de la multiplicación es: " + r);
         System.out.println(m1.toString());
-        sc.close();
-                
         
-
-
+        // Cerrar el Scanner
+        sc.close();
     }
-    
 }
--- Multiplicacion.java
+++ Multiplicacion.java
@@ -1,34 +1,40 @@
-public class Multiplicacion{
-
+public class Multiplicacion {
+    // Variables para los valores y el resultado de la multiplicación
+    private int a, b, r;
     
-    private int a,b,r;
+    // Método para obtener el valor 'a'
     public int getA() {
         return a;
     }
     
+    // Método para establecer el valor 'a'
     public void setA(int a) {
         this.a = a;
     }
     
+    // Método para obtener el valor 'b'
     public int getB() {
         return b;
     }
     
+    // Método para establecer el valor 'b'
     public void setB(int b) {
         this.b = b;
     }
     
+    // Método para realizar la multiplicación de 'a' y 'b'
     public void multiplicar() {
-        this.r = a*b;
+        this.r = a * b;
     }
+    
+    // Método para obtener el resultado de la multiplicación
     public int getR() {
         return r;
     }
     
-    
+    // Método toString para representar la información de la multiplicación
     @Override
     public String toString() {
         return "Multiplicacion [a=" + a + ", b=" + b + ", r=" + r + "]";
     }
-    
 }
